2d7ba9ac81da37e432d92de9aeba0fbfbbd173ce
Balimbanana <31295298+Balimbanana@users.noreply.github.com>
2019-04-22 18:58:46 -0700

    Update syndsinstall.sh
    Update SteamWorks build dl.

--- unix/syndsinstall.sh
+++ unix/syndsinstall.sh
@@ -119,7 +119,7 @@ if [ -d ./$synpath/addons/sourcemod ];then
 fi
 curl -sqL "https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6329-linux.tar.gz" | tar zxvf - -C ./$synpath
 curl -sqL "https://mms.alliedmods.net/mmsdrop/1.10/mmsource-1.10.7-git966-linux.tar.gz" | tar zxvf - -C ./$synpath
-curl -sqL "https://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git126-linux.tar.gz" | tar zxvf - -C ./$synpath
+curl -sqL "https://users.alliedmods.net/~kyles/builds/SteamWorks/SteamWorks-git131-linux.tar.gz" | tar zxvf - -C ./$synpath
 if [ ! -d ./$synpath/addons/sourcemod ];then
 	echo "Failed to auto-install SourceMod, you may have to manually install it."
 	read nullptr
